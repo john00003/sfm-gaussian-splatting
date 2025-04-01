944a3ce4c58cd745cba51741d2bff453c71af27e
john00003 <jetyler@ualberta.ca>
2025-04-01 10:59:21 -0600

    worked on conversion script. WIP.

--- sfm_cpp/src/convert_script.py
+++ sfm_cpp/src/convert_script.py
@@ -112,20 +112,28 @@ def read_images_text(path):
         void Reconstruction::WriteImagesText(const std::string& path)
     """
     images = {}
+    #first = True
     with open(path, "r") as fid:
         while True:
             line = fid.readline()
             if not line:
                 break
             line = line.strip()
+            #print(line)
             if len(line) > 0 and line[0] != "#":
                 elems = line.split()
+                #print(elems)
                 image_id = int(elems[0])
                 qvec = np.array(tuple(map(float, elems[1:5])))
                 tvec = np.array(tuple(map(float, elems[5:8])))
                 camera_id = int(elems[8])
                 image_name = elems[9]
                 elems = fid.readline().split()
+                #if first:
+                #    print(elems)
+                #    print(elems[0::3])
+                #    first = False
+                #print(elems)
                 xys = np.column_stack(
                     [
                         tuple(map(float, elems[0::3])),
@@ -175,6 +183,32 @@ def read_points3D_text(path):
                     point2D_idxs=point2D_idxs,
                 )
     return points3D
+
+def write_next_bytes(fid, data, format_char_sequence, endian_character="<"):
+    """pack and write to a binary file.
+    :param fid:
+    :param data: data to send, if multiple elements are sent at the same time,
+    they should be encapsuled either in a list or a tuple
+    :param format_char_sequence: List of {c, e, f, d, h, H, i, I, l, L, q, Q}.
+    should be the same length as the data list or tuple
+    :param endian_character: Any of {@, =, <, >, !}
+    """
+    if isinstance(data, (list, tuple)):
+        bytes = struct.pack(endian_character + format_char_sequence, *data)
+    else:
+        bytes = struct.pack(endian_character + format_char_sequence, data)
+    fid.write(bytes)
+
+def read_next_bytes(fid, num_bytes, format_char_sequence, endian_character="<"):
+    """Read and unpack the next bytes from a binary file.
+    :param fid:
+    :param num_bytes: Sum of combination of {2, 4, 8}, e.g. 2, 6, 16, 30, etc.
+    :param format_char_sequence: List of {c, e, f, d, h, H, i, I, l, L, q, Q}.
+    :param endian_character: Any of {@, =, <, >, !}
+    :return: Tuple of read and unpacked values.
+    """
+    data = fid.read(num_bytes)
+    return struct.unpack(endian_character + format_char_sequence, data)
 
 
 def write_cameras_binary(cameras, path_to_model_file):
@@ -234,13 +268,110 @@ def write_points3D_binary(points3D, path_to_model_file):
                 write_next_bytes(fid, [image_id, point2D_id], "ii")
 
 
-if __name__ == "__main__":
-    path = ""
-    cameras = read_cameras_text(path + "cameras.txt")
-    images = read_images_text(path + "images.txt")
-    points3D = read_points3D_text(path + "points3D.txt")
+def read_images_binary(path_to_model_file):
+    """
+    see: src/colmap/scene/reconstruction.cc
+        void Reconstruction::ReadImagesBinary(const std::string& path)
+        void Reconstruction::WriteImagesBinary(const std::string& path)
+    """
+    images = {}
+    with open(path_to_model_file, "rb") as fid:
+        num_reg_images = read_next_bytes(fid, 8, "Q")[0]
+        for _ in range(num_reg_images):
+            binary_image_properties = read_next_bytes(
+                fid, num_bytes=64, format_char_sequence="idddddddi"
+            )
+            image_id = binary_image_properties[0]
+            qvec = np.array(binary_image_properties[1:5])
+            tvec = np.array(binary_image_properties[5:8])
+            camera_id = binary_image_properties[8]
+            binary_image_name = b""
+            current_char = read_next_bytes(fid, 1, "c")[0]
+            while current_char != b"\x00":  # look for the ASCII 0 entry
+                binary_image_name += current_char
+                current_char = read_next_bytes(fid, 1, "c")[0]
+            image_name = binary_image_name.decode("utf-8")
+            num_points2D = read_next_bytes(
+                fid, num_bytes=8, format_char_sequence="Q"
+            )[0]
+            x_y_id_s = read_next_bytes(
+                fid,
+                num_bytes=24 * num_points2D,
+                format_char_sequence="ddq" * num_points2D,
+            )
+            xys = np.column_stack(
+                [
+                    tuple(map(float, x_y_id_s[0::3])),
+                    tuple(map(float, x_y_id_s[1::3])),
+                ]
+            )
+            point3D_ids = np.array(tuple(map(int, x_y_id_s[2::3])))
+            images[image_id] = Image(
+                id=image_id,
+                qvec=qvec,
+                tvec=tvec,
+                camera_id=camera_id,
+                name=image_name,
+                xys=xys,
+                point3D_ids=point3D_ids,
+            )
+    return images
 
+def write_images_text(images, path):
+    """
+    see: src/colmap/scene/reconstruction.cc
+        void Reconstruction::ReadImagesText(const std::string& path)
+        void Reconstruction::WriteImagesText(const std::string& path)
+    """
+    if len(images) == 0:
+        mean_observations = 0
+    else:
+        mean_observations = sum(
+            (len(img.point3D_ids) for _, img in images.items())
+        ) / len(images)
+    HEADER = (
+        "# Image list with two lines of data per image:\n"
+        + "#   IMAGE_ID, QW, QX, QY, QZ, TX, TY, TZ, CAMERA_ID, NAME\n"
+        + "#   POINTS2D[] as (X, Y, POINT3D_ID)\n"
+        + "# Number of images: {}, mean observations per image: {}\n".format(
+            len(images), mean_observations
+        )
+    )
+
+    with open(path, "w") as fid:
+        fid.write(HEADER)
+        for _, img in images.items():
+            image_header = [
+                img.id,
+                *img.qvec,
+                *img.tvec,
+                img.camera_id,
+                img.name,
+            ]
+            first_line = " ".join(map(str, image_header))
+            fid.write(first_line + "\n")
+
+            points_strings = []
+            for xy, point3D_id in zip(img.xys, img.point3D_ids):
+                points_strings.append(" ".join(map(str, [*xy, point3D_id])))
+            fid.write(" ".join(points_strings) + "\n")
+
+
+if __name__ == "__main__":
+    path = "../out/build/x64-Release/"
     binary_path = "sparse/0/" # TODO: make sure we don't need to escape the backslash
+
+    #images = read_images_binary("C:\\Users\\doggo\\Downloads\\COLMAP_testing\\south-building\\sparse\\0\\images.bin")
+    #write_images_text(images, "images.txt")
+
+
+    os.makedirs(binary_path, exist_ok=True)
+
+    cameras = read_cameras_text(path + "cameras.txt")
     write_cameras_binary(cameras, os.path.join(binary_path, "cameras.bin"))
+
+    images = read_images_text(path + "images.txt")
     write_images_binary(images, os.path.join(binary_path, "images.bin"))
+
+    points3D = read_points3D_text(path + "points3D.txt")
     write_points3D_binary(points3D, os.path.join(binary_path, "points3D.bin"))
